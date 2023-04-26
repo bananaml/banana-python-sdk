01fbacb13d56ed6d89fcd0d0c5e61ba5a010a62a
erik-dunteman <erikdunteman@gmail.com>
2023-04-25 20:41:14 -0700

    added custom exceptions

--- banana_dev/client.py
+++ banana_dev/client.py
@@ -1,13 +1,23 @@
 from typing import Union
 import requests, time
 
+class ClientException(Exception):
+    "Raised on errors from Banana client"
+    def __init__(self, message = "" , res: requests.Response = None):
+        if res != None:
+            self.message = res.text
+        else:
+            self.message = message
+        self.message = "\n" + self.message
+        super().__init__(self.message)
+
 class Client():
     def __init__(self, api_key, model_key, base_url):
         self.api_key = api_key
         self.model_key = model_key
         self.base_url = base_url
 
-    def call(self, route: str, json: dict = {}, headers: dict = {}, use_replica: Union[str, None] = None, retry_timeout = 300):
+    def call(self, route: str, json: dict = {}, headers: dict = {}, use_replica: Union[str, None] = None, retry=True, retry_timeout = 300):
         headers["Content-Type"] = "application/json"
         headers['X-BANANA-API-KEY'] = self.api_key
         headers['X-BANANA-MODEL-KEY'] = self.model_key
@@ -17,57 +27,66 @@ class Client():
 
         backoff_interval = 0.1 # seed for exponential backoff
         start = time.time()
+        i = 0
         while True:
             if time.time() - start > retry_timeout:
-                raise "call timed out"
+                raise ClientException(message="Retry timeout exceeded")
             
             backoff_interval *= 2
             
             res = requests.post(endpoint, json=json, headers=headers)
-
+            
             # success case -> return json
             if res.status_code == 200:
-                return res.json(), {"headers":res.headers}
-            
+                meta = {"headers":res.headers}
+                try:
+                    return res.json(), meta
+                except:
+                    return {}, meta
+
             # user at their quota -> retry
             elif res.status_code == 400:
+                if not retry:
+                    raise ClientException(res=res)
                 time.sleep(backoff_interval)
                 continue
             
             # bad auth -> error
             elif res.status_code == 401:
-                print(res.content)
-                raise "authorization failed"
-            
+                raise ClientException(res=res)
+
             # potassium endpoint doesn't exist -> error
             elif res.status_code == 404:
-                print(res.content)
-                raise "potassium server returned 404"
+                raise ClientException(res=res)
             
             # payload too large -> error
             elif res.status_code == 413:
-                print(res.content)
-                raise "payload too large"
+                raise ClientException(res=res)
             
             # banana is teapot -> error
             elif res.status_code == 418:
-                raise "banana is a teapot"
+                raise ClientException("banana is a teapot")
             
             # potassium threw thread lock error -> retry
             elif res.status_code == 423:
+                if not retry:
+                    message = res.text
+                    message += "423 errors are returned by Potassium when your server(s) are all busy handling GPU endpoints.\nIn most cases, you just want to retry later. Running banana.call() with the retry=True argument handles this for you."
+                    raise ClientException(message=message)
                 time.sleep(backoff_interval)
                 continue
             
             # banana (or users server) had an unrecoverable error -> error
             elif res.status_code == 500:
-                print(res.content)
-                raise "internal error"
+                if not retry:
+                    raise ClientException(res=res)
             
             # banana had a temporary error -> retry
             elif res.status_code == 503:
+                if not retry:
+                    raise ClientException(res=res)
                 time.sleep(backoff_interval)
                 continue
 
             else:
-                print(res.content)
-                raise "unexpected http response"
+                raise ClientException(message="unexpected http response code: " + res.status_code)
