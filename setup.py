a3359e8de9ac9c1ed5c76f743a898e0f5eb5ae82
erik-dunteman <erikdunteman@gmail.com>
2023-10-06 14:27:31 -0700

    rename model to project

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='banana_dev',
     packages=['banana_dev'],
-    version='5.1.0',
+    version='6.0.0',
     license='MIT',
     # Give a short description about your library
     description='The banana package is a python client to interact with your machine learning models hosted on Banana',
